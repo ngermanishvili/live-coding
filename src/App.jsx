58857a1e473c4cf815c0ad65015ecdf177f65990
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-21 23:49:09 +0400

    pblsh

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,41 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
   const [circles, setCircles] = useState([]);
+
   const colors = ["green", "blue", "orange", "blue", "black", "orange"];
 
   const addCircle = () => {
     const id = Math.floor(Math.random() * colors.length);
+    const color = colors[id];
     const posX = Math.floor(Math.random() * 1024);
     const posY = Math.floor(Math.random() * 766);
-    const color = colors[id];
 
-    setCircles((prev) => [...prev, { id, posX, posY, color }]);
+    const newCircle = { id, posX, posY, color };
+    setCircles((prevCircles) => [...prevCircles, newCircle]);
     console.log(posX, posY);
   };
 
-  const deleteCircle = () => {
+  const deleteCircles = () => {
     setCircles([]);
   };
 
   return (
     <div className="App">
-      <button onClick={() => addCircle()}>REDO</button>
-      <button onClick={() => deleteCircle()}>UNDO</button>
-      <div className="container">
-        {circles.map((circle) => (
-          <div key={circle.id}>
+      <button onClick={addCircle}>Add Circle</button>
+      <button onClick={deleteCircles}>Delete Circles</button>
+      <div>
+        {circles.map(({ id, color, posX, posY }) => (
+          <div key={id}>
             <div
               style={{
-                backgroundColor: circle.color,
+                backgroundColor: color,
                 borderRadius: "50%",
                 width: "15px",
                 height: "15px",
                 position: "absolute",
-                top: circle.posY,
-                left: circle.posX,
+                top: posY,
+                left: posX,
               }}
             ></div>
           </div>
