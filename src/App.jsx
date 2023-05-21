7df33ecde57abaa3b28ddff449a016e133a3cb84
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-21 23:37:23 +0400

    live code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [circles, setCircles] = useState([]);
+  const colors = ["green", "blue", "orange", "blue", "black", "orange"];
+
+  const addCircle = () => {
+    const id = Math.floor(Math.random() * colors.length);
+    const posX = Math.floor(Math.random() * 1024);
+    const posY = Math.floor(Math.random() * 766);
+    const color = colors[id];
+
+    setCircles((prev) => [...prev, { id, posX, posY, color }]);
+    console.log(posX, posY);
+  };
+
+  const deleteCircle = () => {
+    setCircles([]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <button onClick={() => addCircle()}>REDO</button>
+      <button onClick={() => deleteCircle()}>UNDO</button>
+      <div className="container">
+        {circles.map((circle) => (
+          <div key={circle.id}>
+            <div
+              style={{
+                backgroundColor: circle.color,
+                borderRadius: "50%",
+                width: "15px",
+                height: "15px",
+                position: "absolute",
+                top: circle.posY,
+                left: circle.posX,
+              }}
+            ></div>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
